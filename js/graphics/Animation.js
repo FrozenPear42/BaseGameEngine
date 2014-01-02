ba737ed2a6e13a55fdb2458c8727803871d85b64
Wojciech Gruszka <wojciech@gruszka.eu>
2014-01-02 21:37:04 +0100

    Linear move animation finally works.

--- js/graphics/Animation.js
+++ js/graphics/Animation.js
@@ -1,14 +1,12 @@
 var Animation = (function()
 {
-	
+
 	var animations = [];
 	var intervals = [];
 
-	function cancelAnimation(key)
+	function cancelAnimation(anim)
 	{
-		clearInterval(intervals[key]);
-		intervals[key] = null;
-
+		animmations.removeElement(anim);
 	}
 
 	function animMoveTo(obj, toX, toY, time, func, callback)
@@ -18,67 +16,53 @@ var Animation = (function()
 
 		var dX = toX - startX;
 		var dY = toY - startY;
-		
-		var vX = dX/time;
-		var vY = dY/time;
-		
+
+		var vX = dX / time;
+		var vY = dY / time;
+
 		var steps;
-			if (Math.abs(dX) > Math.abs(dY))
-				steps = Math.abs(dX)
-			else
-				steps = Math.abs(dY);
-		
+		if (Math.abs(dX) > Math.abs(dY))
+			steps = Math.abs(dX)
+		else
+			steps = Math.abs(dY);
+
 		var cnt = 0;
 
 		console.log("vX", vX);
 		console.log("vY", vY);
 		console.log("steps", steps);
-		console.log("interval", (time*1000)/steps); 
+		console.log("interval", (time * 1000) / steps);
 
-		animations.push({
-		obj : obj,
-		time : time,
-		func : func,
-		vX : vX,
-		vY : vY,
-		steps : steps,
-		cnt : 0
-		});
-		
-		/*intervals[obj] = setInterval(function()
+		return animations.push(
 		{
-			cnt++;
-			if (cnt >= steps)
-			{
-				cancelAnimation(obj);
-				//callback();
-			}
-			
-			obj.x += vX*(time/steps);
-			obj.y += vY*(time/steps);
-			
-		}, (time*1000)/steps);
-	*/
-	}
+			obj : obj,
+			time : time,
+			func : func,
+			vX : vX,
+			vY : vY,
+			steps : steps,
+			cnt : 0
+		});
 
 	function process(time)
 	{
-		animations.forEach(function(anim){
-		//console.log(anim);
-		//anim.cnt += (time/anim.steps);
-		anim.cnt += time*(anim.time/anim.steps);
-		if (anim.cnt >= anim.steps)
+		animations.forEach(function(anim)
 		{
-			console.log(anim);
-			animations.removeElement(anim);
-			return;
-		}
-		anim.obj.x += anim.vX * (time/1000);
-		anim.obj.y += anim.vY * (time/1000);
-		
+
+			anim.cnt += time/((anim.time * 1000) / anim.steps);
+
+			if (anim.cnt >= anim.steps)
+			{
+				console.log(anim);
+				animations.removeElement(anim);
+				return;
+			}
+			anim.obj.x += anim.vX * (time / 1000);
+			anim.obj.y += anim.vY * (time / 1000);
+
 		});
 	}
-	
+
 	return {
 		process : process,
 		animMoveTo : animMoveTo,
